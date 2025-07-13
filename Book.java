fbd77e26be16f6f2b7cf8545d3c9fa1b2ce088fd
Pooja Sahu <114080807+pooja3651@users.noreply.github.com>
2025-07-13 13:07:03 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,88 @@
+package com.pooja.library.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-increment primary key
+    private Integer id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String author;
+
+    private String isbn;
+
+    private Boolean available;
+
+    // ----- Constructors -----
+
+    public Book() {
+    }
+
+    public Book(Integer id, String title, String author, String isbn, Boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.available = available;
+    }
+
+    // ----- Getters and Setters -----
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", available=" + available +
+                '}';
+    }
+}
